f2d5750c72a0e4e6122bad46b01dc56a36c74a0c
Franpall <98850575+Franpall@users.noreply.github.com>
2024-08-25 16:09:05 -0400

    PRUEBAS GENERALES

--- flaskr/pruebaTemplateInicio.py
+++ flaskr/pruebaTemplateInicio.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     usuario_logeado = True
-    productos = [{"nombre":"xiaomi mi 10", "precio":"1000"},{"nombre":"xiaomi retmi 49", "precio":"600"},{"nombre":"poco x5 pro", "precio":"200"}, {"nombre":"xiaomi mi 10", "precio":"1000"},{"nombre":"xiaomi retmi 49", "precio":"600"},{"nombre":"poco x5 pro", "precio":"200"}]
+    productos = [{"nombre":"xiaomi mi 10", "precio":"1000","cantidad":"2"},{"nombre":"xiaomi retmi 49", "precio":"600","cantidad":"2"},{"nombre":"poco x5 pro", "precio":"200","cantidad":"2"}, {"nombre":"xiaomi mi 10", "precio":"1000","cantidad":"2"},{"nombre":"xiaomi retmi 49", "precio":"600","cantidad":"2"},{"nombre":"poco x5 pro", "precio":"200","cantidad":"2"}]
     return render_template('blog/Inicio.html', usuario_logeado=usuario_logeado, productos=productos)
+
+@app.route("/carrito")
+def cart():
+    usuario_logeado = True
+    productos = [{"nombre":"xiaomi mi 10", "precio":"1000","cantidad":"2"},{"nombre":"xiaomi retmi 49", "precio":"600","cantidad":"2"},{"nombre":"poco x5 pro", "precio":"200","cantidad":"2"}, {"nombre":"xiaomi mi 10", "precio":"1000","cantidad":"2"},{"nombre":"xiaomi retmi 49", "precio":"600","cantidad":"2"},{"nombre":"poco x5 pro", "precio":"200","cantidad":"2"}]
+    return render_template('blog/carrito.html', usuario_logeado=usuario_logeado, items=productos)
 
 if __name__ == '__main__':
     app.run(debug=True)
